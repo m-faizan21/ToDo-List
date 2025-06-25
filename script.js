3f5513c49b1fc85a87fd6a10c54267e7fad4291b
Faizan <mohdfaizancs@gmail.com>
2025-06-25 17:15:48 +0530

    Added More Functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask(columnId) {
+    const input = document.getElementById(`${columnId}-input`);
+    const taskText = input.value;
+   
+
+    if(taskText ==""){
+        return;
+    }
+    const taskElement = createTaskElement(taskText);
+    document.getElementById(`${columnId}-tasks`).appendChild(taskElement);
+    input.value = ""; // Clear the input field after adding the task
+}
+
+function createTaskElement(taskText) {
+    const taskElement = document.createElement("div");
+    taskElement.textContent = taskText;
+    taskElement.classList.add("card");
+    return taskElement;
+
+}
